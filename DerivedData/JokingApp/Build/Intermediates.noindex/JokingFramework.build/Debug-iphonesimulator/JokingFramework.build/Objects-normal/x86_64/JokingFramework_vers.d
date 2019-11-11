aa0d0e1f083e306aa770646e17c80765172a368f
Faisal Rahman <faze93@live.com>
2019-11-11 18:43:01 +0000

    remove derived data directory

--- DerivedData/JokingApp/Build/Intermediates.noindex/JokingFramework.build/Debug-iphonesimulator/JokingFramework.build/Objects-normal/x86_64/JokingFramework_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/rahmaf04/Desktop/JokingApp/DerivedData/JokingApp/Build/Intermediates.noindex/JokingFramework.build/Debug-iphonesimulator/JokingFramework.build/DerivedSources/JokingFramework_vers.c
